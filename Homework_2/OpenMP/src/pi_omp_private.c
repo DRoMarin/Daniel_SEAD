8013164a5dc5ba426c38f454073c9af0c36b95c5
Daniel Rojas <159dram@gmail.com>
2023-06-13 19:02:01 -0600

    testing OpenMP private, need to add comments

--- Homework_2/OpenMP/src/pi_omp_private.c
+++ Homework_2/OpenMP/src/pi_omp_private.c
@@ -19,11 +19,14 @@ void main() {
     step = 1.0 / (double)num_steps;
 
     start_time = omp_get_wtime();
+    #pragma omp parallel
 
-
-    for (i = 1; i <= num_steps; i++) {
-        x = (i - 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
+    {
+        #pragma omp for reduction(+:sum) private(x)
+        for (i = 1; i <= num_steps; i++) {
+            x = (i - 0.5) * step;
+            sum = sum + 4.0 / (1.0 + x * x);
+        }
     }
 
     pi = step * sum;
